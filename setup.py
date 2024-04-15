13f9d21c4a3ad998c725799d6bcc4fd9301d63dd
s1rne <s.simarano8@gmail.com>
2024-04-15 22:02:47 +0300

    to 2.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='AsyncKandinsky',
-    version='2.0.0',
+    version='2.0.1',
     author='s1rne',
     author_email='s.simaranov8@gmail.com',
     description='This module is designed for asynchronous use of the kandinsky neural network and easy integration into your project.',
